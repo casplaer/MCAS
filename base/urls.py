f737689a156fc77a9d4885ee546afe298f46b2ec
Slava <73721625+casplaer@users.noreply.github.com>
2023-04-08 20:52:41 +0300

    Add files via upload

---
+++ base/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.loginPage, name = 'login'),
+    path('logout/', views.logoutUser, name = 'logout'),
+    path('register/', views.registerPage, name = 'register'),
+    path('profile/<str:pk>/', views.userProfile, name = 'user-profile'),
+
+    path('', views.home, name = 'home'),
+    path('new/<str:pk>/', views.new, name = 'new'),
+    path('create-new/', views.createNew, name = 'create-new'),
+    path('news/', views.newsPage, name = 'news'),
+]
